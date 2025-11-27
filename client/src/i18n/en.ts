a0895ddb6e41ac368bc256f733dda0015177d3cc
Johannes S <johannesstruweg@gmail.com>
2025-11-27 22:08:38 +0100

    Update en.ts

--- client/src/i18n/en.ts
+++ client/src/i18n/en.ts
@@ -1,5 +1,6 @@
 export const en = {
   heroLine1: "Let's make work",
   heroLine2: "feel like less work.",
+   heroDescription: "Independent strategy and AI automation studio in Norway. We move fast and combine strategy, design, and intelligent automation to make growth measurable and sustainable.",
   contactTitle: "Scale faster than ever, guaranteed results.",
 };
